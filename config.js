1a584485fcdfbdbfd0e9a752dcb7e0cf15f153d7
jekiss1016 <jekiss1016@gmail.com>
2026-06-07 15:15:31 -0700

    Configure Supabase endpoints in config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
  */
 
 window.SUPABASE_CONFIG = {
-  url: "YOUR_SUPABASE_PROJECT_URL",       // E.g. "https://xyzexample.supabase.co"
-  anonKey: "YOUR_SUPABASE_ANON_KEY"     // E.g. "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+  url: "https://xghcwcgbzdzlupotkyep.supabase.co",       // E.g. "https://xyzexample.supabase.co"
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnaGN3Y2diemR6bHVwb3RreWVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4NTY1MzQsImV4cCI6MjA5NjQzMjUzNH0.sxoJ7JQ4clF-rzX3JQHuT6gpu-JycG6AM5Go68SK-Hg"     // E.g. "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
 };
